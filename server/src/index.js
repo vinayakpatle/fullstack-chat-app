1baf9bce56b00f9e3dda25cff8cf4e45935da0b3
Vinayak patle <vinayakpatle2@gmail.com>
2025-02-25 03:12:39 +0530

    update in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -22,13 +22,13 @@ app.use(cookieParser());
 app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"fronted/dist")));
-
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"fronted/dist/index.html"));
-    })
-}
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 const PORT=process.env.PORT;
 
