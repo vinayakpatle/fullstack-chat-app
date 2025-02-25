a08d17701b5f6b16c2478ade7e878b35ea5f8bde
Vinayak patle <vinayakpatle2@gmail.com>
2025-02-25 16:07:15 +0530

    when user logout selecetedUser need to be null that update

--- fronted/src/store/useAuthStore.js
+++ fronted/src/store/useAuthStore.js
@@ -2,6 +2,7 @@ import {create} from "zustand";
 import {axiosInstance} from '../lib/axios.js';
 import {toast} from "react-hot-toast";
 import {io} from 'socket.io-client';
+import { useChatStore } from "./useChatStore.js";
 
 const BASE_URL=import.meta.env.MODE==="development" ? "http://localhost:5001" : "/";
 
@@ -64,6 +65,7 @@ export const useAuthStore=create((set,get)=>({
         try{
             await axiosInstance.post("/auth/logout")
             set({authUser:null});
+            useChatStore.setState({selectedUser:null});
             toast.success("Logged out successfully");
 
             get().disconnectSocket();
